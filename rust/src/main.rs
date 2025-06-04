d772fad2a53ab305d09cbad151c1b526e0781553
samic++ <samichentouf+git2@gmail.com>
2025-06-04 21:29:05 +0200

    make the tests multi threaded

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,7 @@
 mod sort;
 mod shuffle;
 use std::time::Instant;
+use std::thread;
 
 fn main() {
     println!("Hello, world!");
@@ -9,35 +10,53 @@ fn main() {
 }
 
 fn test_sorts(){
-    let sv=&shuffle::shuffled_uint32(10_000);
+    let sv=&shuffle::shuffled_uint32(30_000);
     println!("{:?}\n",&sv[0..20]);
 
     // quick sort
-    let b=Instant::now();
-    let qs=sort::quick::number(sv.clone());
-    let d = b.elapsed();
-    println!("quick sort done {:?}",&qs[0..20]);
-    println!("elapsed {}µs",d.as_nanos()as f64 / 1000.0);
-    println!("elapsed {}ms",d.as_micros()as f64 / 1000.0);
-    println!("\n");
+    {
+        let svc=sv.clone();
+        thread::spawn(move || {
+            let b=Instant::now();
+            let qs=sort::quick::number(svc);
+            let d = b.elapsed();
+            println!("quick sort done {:?}",&qs[0..20]);
+            println!("elapsed {}µs",d.as_nanos()as f64 / 1000.0);
+            println!("elapsed {}ms",d.as_micros()as f64 / 1000.0);
+            println!("\n");
+        });
+    };
 
     // bubble sort
-    let b=Instant::now();
-    let qs=sort::bubble::number(sv.clone());
-    let d = b.elapsed();
-    println!("bubble sort done {:?}",&qs[0..20]);
-    println!("elapsed {}µs",d.as_nanos()as f64 / 1000.0);
-    println!("elapsed {}ms",d.as_micros()as f64 / 1000.0);
-    println!("\n");
+    {
+        let svc=sv.clone();
+        thread::spawn(move || {
+            let b=Instant::now();
+            let qs=sort::bubble::number(svc);
+            let d = b.elapsed();
+            println!("bubble sort done {:?}",&qs[0..20]);
+            println!("elapsed {}µs",d.as_nanos()as f64 / 1000.0);
+            println!("elapsed {}ms",d.as_micros()as f64 / 1000.0);
+            println!("\n");
+        });
+    };
 
     // merge sort
-    let b=Instant::now();
-    let qs=sort::merge::number(sv.clone());
-    let d = b.elapsed();
-    println!("merge sort done {:?}",&qs[0..20]);
-    println!("elapsed {}µs",d.as_nanos()as f64 / 1000.0);
-    println!("elapsed {}ms",d.as_micros()as f64 / 1000.0);
-    println!("\n");
+    {
+        let svc=sv.clone();
+        thread::spawn(move || {
+            let b=Instant::now();
+            let qs=sort::merge::number(svc);
+            let d = b.elapsed();
+            println!("merge sort done {:?}",&qs[0..20]);
+            println!("elapsed {}µs",d.as_nanos()as f64 / 1000.0);
+            println!("elapsed {}ms",d.as_micros()as f64 / 1000.0);
+            println!("\n");
+        });
+    };
 
+    loop {};
+    // while done!=3{}
+    // print!("done == 3");
     
 }
