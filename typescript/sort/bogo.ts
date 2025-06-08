16896a6802805e26a383ca0f73b8788a9c9f64dc
samic++ <samichentouf+git2@gmail.com>
2025-06-08 18:33:02 +0200

    start adding debug tools to deno

--- typescript/sort/bogo.ts
+++ typescript/sort/bogo.ts
@@ -15,3 +15,38 @@ export const bogoSort=new class BogoSort{
         return sorted;
     }
 };
+
+export const bogoSortDebug=new class BogoSortDebug{
+    workArr:number[]=[];
+    async next(){return new Promise(r=>setTimeout(r))};
+    async checked(){return new Promise(r=>setTimeout(r))};
+    async get(name:number){return this.workArr[name]}
+    async set(name:number,value:number){return this.workArr[name]=value}
+
+    async number(arr: number[]):Promise<number[]>{
+        this.workArr=[...arr];
+        while(true){
+            this.workArr=shuffle(arr);
+            await this.next();
+            await this.checked();
+            if(check.number(this.workArr)){
+                break;
+            };
+        }
+        return this.workArr;
+    }
+};
+
+/*
+export const SortDebug=new class SortDebug{
+    workArr:number[]=[];
+    async next(){return new Promise(r=>setTimeout(r))};
+    async checked(){return new Promise(r=>setTimeout(r))};
+    async get(name:number){return this.workArr[name]}
+    async set(name:number,value:number){return this.workArr[name]=value}
+
+    async number(arr: number[]):Promise<number[]>{
+        ;
+    }
+};
+// */
--- typescript/sort/bubble.ts
+++ typescript/sort/bubble.ts
@@ -23,4 +23,31 @@ export const bubbleSort=new class BubbleSort{
         
         return narr;
     }
-}
+};
+
+export const bubbleSortDebug=new class BubbleSortDebug{
+    workArr:number[]=[];
+    async next(){return new Promise(r=>setTimeout(r))};
+    async checked(){return new Promise(r=>setTimeout(r))};
+    async get(name:number){return this.workArr[name]}
+    async set(name:number,value:number){return this.workArr[name]=value}
+
+    async number(arr: number[]):Promise<number[]>{
+        const narr=this.workArr=[...arr];
+        
+        for(let i=0;i<narr.length;i++){
+            for(let j=0;j<narr.length;j++){
+                //console.log(narr[j]>narr[j+1],narr[j],narr[j+1])
+                if(narr[j]>narr[j+1]){
+                    //console.log("if statement triggered");
+                    const t=await this.get(j);
+                    await this.set(j,j+1);
+                    await this.set(j+1,t);
+                }
+                //console.log("fixed",narr[j]<narr[j+1])
+            }
+        }
+        
+        return narr;
+    }
+};
