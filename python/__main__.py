3a51136089015ad6fa9530b1b0a515591f70816f
samic++ <samichentouf+git2@gmail.com>
2025-06-08 14:23:06 +0000

    add python ffi bindigs

--- python/__main__.py
+++ python/__main__.py
@@ -1,1 +1,13 @@
+import binding
+import random
+
 print("name = "+__name__)
+
+print("testing with shuffled array")
+arr=list(range(100))
+random.shuffle(arr)
+print(arr)
+sarr=binding.sort("quick_sort",arr)
+print("\nsorted array")
+print(sarr)
+
---
+++ python/binding.py
@@ -0,0 +1,18 @@
+import ctypes
+import os
+from typing import List
+
+workdir:str=os.path.dirname(os.path.realpath(__file__))
+sortlib:ctypes.CDLL=ctypes.CDLL(workdir+"/../target/release/libsorting_algorithm.so")
+
+for i in ["quick_sort","radix_sort","heap_sort","merge_sort","bubble_sort","bogo_sort","stalin_sort"]:
+    func=getattr(sortlib,i)
+    func.argtypes=[ctypes.POINTER(ctypes.c_int),ctypes.c_size_t]
+    func.restype=None
+    pass
+
+def sort(name:str,data:List[int]):
+    arr=(ctypes.c_int*len(data))(*data)
+    getattr(sortlib,name)(arr,len(data))
+    return list(arr)
+
