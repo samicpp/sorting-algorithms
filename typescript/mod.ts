e3492bf189962da6c1a94608bcf73b0b9a1052d5
samic++ <samichentouf+git2@gmail.com>
2025-06-09 15:36:03 +0200

    fix exports

--- typescript/mod.ts
+++ typescript/mod.ts
@@ -1,12 +1,12 @@
 //import "./lib.deno.d.ts";
 
-export { mergeSort } from "./sort/merge.ts";
-export { quickSort } from "./sort/quick.ts";
-export { radixSort } from "./sort/radix.ts";
-export { heapSort } from "./sort/heap.ts";
-export { bogoSort } from "./sort/bogo.ts";
-export { bubbleSort } from "./sort/bubble.ts";
-export { stalinSort } from "./sort/stalin.ts";
+export * from "./sort/merge.ts";
+export * from "./sort/quick.ts";
+export * from "./sort/radix.ts";
+export * from "./sort/heap.ts";
+export * from "./sort/bogo.ts";
+export * from "./sort/bubble.ts";
+export * from "./sort/stalin.ts";
 export * as check from "./sort/check.ts";
 
 
